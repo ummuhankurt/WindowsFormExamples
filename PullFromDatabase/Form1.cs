6dd27d054bf6258f57040f275f0302e382415d25
Ümmühan Kurt <63405463+ummuhankurt@users.noreply.github.com>
2022-10-18 13:50:08 +0300

    Add project files.

---
+++ PullFromDatabase/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace PullFromDatabase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection(@"Data source=UMMUHANKURT; Initial Catalog=FilmArsiv;Integrated Security=True");
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            baglanti.Open();
+
+
+
+            SqlCommand komut = new SqlCommand("select *from Filmler", baglanti);
+            var reader = komut.ExecuteReader();
+            DataTable table = new DataTable();
+            table.Load(reader);
+            dataGridView1.DataSource = table;
+            baglanti.Close();
+            MessageBox.Show("Geldi");
+        }
+    }
+}
