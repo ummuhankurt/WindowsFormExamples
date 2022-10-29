33983111db8b3127102cd9b6aca315b645b1ec6b
Ümmühan Kurt <63405463+ummuhankurt@users.noreply.github.com>
2022-10-29 22:45:37 +0300

    .

---
+++ ConditionalColoringWithDataGridView/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace ConditionalColoringWithDataGridView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.personellerDataSet = new ConditionalColoringWithDataGridView.PersonellerDataSet();
+            this.perInformationBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.perInformationTableAdapter = new ConditionalColoringWithDataGridView.PersonellerDataSetTableAdapters.PerInformationTableAdapter();
+            this.perIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.perAdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.perSoyadDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.perSehirDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.perMaasDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.perDurumDataGridViewCheckBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.perYasDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personellerDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.perInformationBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.perIdDataGridViewTextBoxColumn,
+            this.perAdDataGridViewTextBoxColumn,
+            this.perSoyadDataGridViewTextBoxColumn,
+            this.perSehirDataGridViewTextBoxColumn,
+            this.perMaasDataGridViewTextBoxColumn,
+            this.perDurumDataGridViewCheckBoxColumn,
+            this.perYasDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.perInformationBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(73, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(756, 390);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // personellerDataSet
+            // 
+            this.personellerDataSet.DataSetName = "PersonellerDataSet";
+            this.personellerDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // perInformationBindingSource
+            // 
+            this.perInformationBindingSource.DataMember = "PerInformation";
+            this.perInformationBindingSource.DataSource = this.personellerDataSet;
+            // 
+            // perInformationTableAdapter
+            // 
+            this.perInformationTableAdapter.ClearBeforeFill = true;
+            // 
+            // perIdDataGridViewTextBoxColumn
+            // 
+            this.perIdDataGridViewTextBoxColumn.DataPropertyName = "PerId";
+            this.perIdDataGridViewTextBoxColumn.HeaderText = "PerId";
+            this.perIdDataGridViewTextBoxColumn.Name = "perIdDataGridViewTextBoxColumn";
+            this.perIdDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // perAdDataGridViewTextBoxColumn
+            // 
+            this.perAdDataGridViewTextBoxColumn.DataPropertyName = "PerAd";
+            this.perAdDataGridViewTextBoxColumn.HeaderText = "PerAd";
+            this.perAdDataGridViewTextBoxColumn.Name = "perAdDataGridViewTextBoxColumn";
+            // 
+            // perSoyadDataGridViewTextBoxColumn
+            // 
+            this.perSoyadDataGridViewTextBoxColumn.DataPropertyName = "PerSoyad";
+            this.perSoyadDataGridViewTextBoxColumn.HeaderText = "PerSoyad";
+            this.perSoyadDataGridViewTextBoxColumn.Name = "perSoyadDataGridViewTextBoxColumn";
+            // 
+            // perSehirDataGridViewTextBoxColumn
+            // 
+            this.perSehirDataGridViewTextBoxColumn.DataPropertyName = "PerSehir";
+            this.perSehirDataGridViewTextBoxColumn.HeaderText = "PerSehir";
+            this.perSehirDataGridViewTextBoxColumn.Name = "perSehirDataGridViewTextBoxColumn";
+            // 
+            // perMaasDataGridViewTextBoxColumn
+            // 
+            this.perMaasDataGridViewTextBoxColumn.DataPropertyName = "PerMaas";
+            this.perMaasDataGridViewTextBoxColumn.HeaderText = "PerMaas";
+            this.perMaasDataGridViewTextBoxColumn.Name = "perMaasDataGridViewTextBoxColumn";
+            // 
+            // perDurumDataGridViewCheckBoxColumn
+            // 
+            this.perDurumDataGridViewCheckBoxColumn.DataPropertyName = "PerDurum";
+            this.perDurumDataGridViewCheckBoxColumn.HeaderText = "PerDurum";
+            this.perDurumDataGridViewCheckBoxColumn.Name = "perDurumDataGridViewCheckBoxColumn";
+            // 
+            // perYasDataGridViewTextBoxColumn
+            // 
+            this.perYasDataGridViewTextBoxColumn.DataPropertyName = "PerYas";
+            this.perYasDataGridViewTextBoxColumn.HeaderText = "PerYas";
+            this.perYasDataGridViewTextBoxColumn.Name = "perYasDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personellerDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.perInformationBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private PersonellerDataSet personellerDataSet;
+        private System.Windows.Forms.BindingSource perInformationBindingSource;
+        private PersonellerDataSetTableAdapters.PerInformationTableAdapter perInformationTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn perIdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn perAdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn perSoyadDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn perSehirDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn perMaasDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn perDurumDataGridViewCheckBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn perYasDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ ConditionalColoringWithDataGridView/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConditionalColoringWithDataGridView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'personellerDataSet.PerInformation' table. You can move, or remove it, as needed.
+            this.perInformationTableAdapter.Fill(this.personellerDataSet.PerInformation);
+            for (int i = 0; i < dataGridView1.Rows.Count-1; i++)
+            {
+                DataGridViewCellStyle renk = new DataGridViewCellStyle();
+                if((bool)dataGridView1.Rows[i].Cells[5].Value == true)
+                {
+                    renk.BackColor = Color.Pink;
+                    renk.ForeColor = Color.Black;
+                }
+                else
+                {
+                    renk.BackColor = Color.FloralWhite;
+                    renk.ForeColor = Color.Black;
+                }
+                dataGridView1.Rows[i].DefaultCellStyle = renk; // varsayılan hücre biçimi olarak renkten gelen değerleri uygula.
+            }
+
+        }
+    }
+}
