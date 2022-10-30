d6ed7ea6bb9f63c6204310e4d97d7ad5651fce48
Ümmühan Kurt <63405463+ummuhankurt@users.noreply.github.com>
2022-10-30 19:46:42 +0300

    .

---
+++ PreventRepetitiveRecording/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace PreventRepetitiveRecording
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtNumara = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(106, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad Soyad : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(119, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numara :";
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtAdSoyad.Location = new System.Drawing.Point(216, 98);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(166, 26);
+            this.txtAdSoyad.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(216, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 26);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Kaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNumara
+            // 
+            this.txtNumara.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtNumara.Location = new System.Drawing.Point(216, 46);
+            this.txtNumara.Name = "txtNumara";
+            this.txtNumara.Size = new System.Drawing.Size(166, 26);
+            this.txtNumara.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 268);
+            this.Controls.Add(this.txtNumara);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MaskedTextBox txtNumara;
+    }
+}
+
---
+++ PreventRepetitiveRecording/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace PreventRepetitiveRecording
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection(@"Data Source=UMMUHANKURT;Initial Catalog=DersVeriTabani;Integrated Security=True");
+        bool durum;
+        void mukerrer()
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("select *from bilgi where Numara = @p1", baglanti);
+            komut.Parameters.AddWithValue("@p1", txtNumara.Text);
+            SqlDataReader sqlDataReader = komut.ExecuteReader();
+            if (sqlDataReader.Read()) // @p1 değeri veri tabanında varsa
+            {
+                durum = false; // veritabanında böyle bir kayıt var, yenisini ekleme.
+            }
+            else
+            {
+                durum = true; // veritabanında böyle bir kayıt yok, yenisini ekleyebilirsin.
+            }
+            baglanti.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mukerrer();
+            if (durum)
+            {
+                baglanti.Open();
+                SqlCommand komut = new SqlCommand("Insert into bilgi values(@p1,@p2)", baglanti);
+                komut.Parameters.AddWithValue("@p1", txtNumara.Text);
+                komut.Parameters.AddWithValue("@p2", txtAdSoyad.Text);
+                komut.ExecuteNonQuery();
+                baglanti.Close();
+                MessageBox.Show("Kayıt eklendi");
+            }
+            else
+            {
+                MessageBox.Show("Bu kayıt zaten var","Bilgi",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
