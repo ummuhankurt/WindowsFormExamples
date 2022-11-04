bdd4783d19c23be12161af8ad6a7001dde8c8270
Ümmühan Kurt <63405463+ummuhankurt@users.noreply.github.com>
2022-11-04 19:01:27 +0300

    .

---
+++ OneToOneRelationship/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace OneToOneRelationship
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection("Data Source=.;Initial Catalog=Ders;Integrated Security=True");
+
+        void Notlar()
+        {
+            SqlCommand komut = new SqlCommand("Select *from Notlar",baglanti);
+            SqlDataAdapter da = new SqlDataAdapter(komut);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("insert into Ogrenciler values (@p1 , @p2)",baglanti);
+            komut.Parameters.AddWithValue("@p1", textBox1.Text);
+            komut.Parameters.AddWithValue("@p2", textBox2.Text);
+            komut.ExecuteNonQuery();
+            MessageBox.Show("Öğrenci eklendi");
+            Notlar();
+            baglanti.Close();
+        }
+    }
+}
