9a569397cb7ead5059810c1c54970afe7c552476
Ümmühan Kurt <63405463+ummuhankurt@users.noreply.github.com>
2022-11-07 14:20:37 +0300

    .

---
+++ CreateQRCode/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MessagingToolkit.QRCode.Codec;
+
+namespace CreateQRCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QRCodeEncoder enc = new QRCodeEncoder();
+            pictureBox1.Image = enc.Encode(textBox1.Text); // Encode :> sıkıştırmak. Compress.
+        }
+    }
+}
