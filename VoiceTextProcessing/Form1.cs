06d61c9e90cf6e278b4cccf1aa3a3a2ada770f7e
Ümmühan Kurt <63405463+ummuhankurt@users.noreply.github.com>
2022-11-07 14:44:13 +0300

    .

---
+++ VoiceTextProcessing/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SpeechLib;
+
+namespace VoiceTextProcessing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SpVoice spVoice = new SpVoice();
+            spVoice.Speak(richTextBox1.Text);
+        }
+    }
+}
