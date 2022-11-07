918a9a358f721287d4a060078a8a8ddd502444e0
Ümmühan Kurt <63405463+ummuhankurt@users.noreply.github.com>
2022-11-07 18:10:33 +0300

    .

--- DecodeQR/Form1.cs
+++ DecodeQR/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MessagingToolkit.QRCode.Codec;
+using MessagingToolkit.QRCode.Codec.Data;
 
 namespace DecodeQR
 {
@@ -15,6 +17,26 @@ namespace DecodeQR
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QRCodeEncoder qr = new QRCodeEncoder();
+            pictureBox1.Image = qr.Encode(textBox1.Text);
+            textBox1.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            QRCodeDecoder deQr = new QRCodeDecoder();
+            textBox1.Text = (deQr.Decode(new QRCodeBitmapImage(pictureBox1.Image as Bitmap)));
+        }
+
+        private void button3_Click(object sender, EventArgs e)   
+        {
+            openFileDialog1.ShowDialog();
+            pictureBox1.ImageLocation = openFileDialog1.FileName;
+
         }
     }
 }
